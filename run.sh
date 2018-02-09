2eb6fd6a228f43b2bd9c4bc70b7dcda95f2ee43b
Денис Туляков <dtulyakov@gmail.com>
2018-02-09 22:18:58 +0300

    изменено:      run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ HTMLFILE=$(cd ${WORKSPACE} && ls *.html)
 for htmlf in ${HTMLFILE}
     do
     python -mrcssmin -p < ${WORKSPACE}/${htmlf} > ${WORKSPACE}/../../${htmlf}
+    rm ${WORKSPACE}/${htmlf}
+    mv ${WORKSPACE}/../../${htmlf} ${WORKSPACE}/${htmlf}
 done
